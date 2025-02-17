30397e69d303a0f901b4ee2e12864126876e49b5
gauravrawl <gauravrawal3139@gmail.com>
2025-02-17 11:49:00 +0530

    logo added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <Image
+                               src="/images/logo-web.png"
+                               alt="logo"
+                               className="rounded-xl"
+                               width={70}
+                               height={70}
+                             />
+        <p className="text-slate-600">&copy; 2025 All rights reserved.</p>
       </div>
     </footer>
   );
