835cf6b44f9bc14eb8721a9606ebbabba62e758d
gauravrawl <gauravrawal3139@gmail.com>
2025-02-17 13:26:09 +0530

    mail added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,13 +4,13 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-12 flex justify-between items-center">
         <Image
                                src="/images/logo-web.png"
                                alt="logo"
                                className="rounded-xl"
-                               width={70}
-                               height={70}
+                               width={40}
+                               height={40}
                              />
         <p className="text-slate-600">&copy; 2025 All rights reserved.</p>
       </div>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,9 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              // href="/#contact"
+              href={`mailto:${'gauravrawal3139@gmail.com'}?subject=${'Interested in Hiring You'}&body=${"Hi Gaurav,\n\nI would like to discuss a potential opportunity with you."}`}
+
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hire Me
